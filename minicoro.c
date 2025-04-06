c2fc2f51c433a9067e7c7033eff11af4f7c51076
Jake Fecher <jake@aztecprotocol.com>
2025-04-06 12:48:06 -0500

    Define some helpers

--- minicoro.c
+++ minicoro.c
@@ -1,2 +1,13 @@
 #define MINICORO_IMPL
 #include "minicoro.h"
+
+char mco_coro_is_suspended(mco_coro* k) {
+    return mco_status(k) == MCO_SUSPENDED;
+}
+
+mco_coro* mco_coro_init(void (*effectful_fn)(mco_coro* co)) {
+    mco_coro* co;
+    mco_desc desc = mco_desc_init(effectful_fn, 0);
+    mco_result res = mco_create(&co, &desc);
+    return co;
+}
