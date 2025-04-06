3d593fecacc5c4e6b958860529ce2352e7bb5708
Jake Fecher <jake@aztecprotocol.com>
2025-04-06 14:54:10 -0500

    Remove underscore functions

--- minicoro.c
+++ minicoro.c
@@ -9,11 +9,6 @@ mco_coro* mco_coro_init(void (*effectful_fn)(mco_coro* co)) {
     assert(res == MCO_SUCCESS);
     return co;
 }
-// For some reason rust requires a leading underscore on these names
-// so we define both versions. These versions are only used within the cranelift JIT.
-mco_coro* _mco_coro_init(void (*effectful_fn)(mco_coro* co)) {
-    return mco_coro_init(effectful_fn);
-}
 
 char mco_coro_free(mco_coro* k) {
     mco_result res = mco_destroy(k);
@@ -23,15 +18,9 @@ char mco_coro_free(mco_coro* k) {
     // to return unit values we still need to return one here.
     return 0;
 }
-char _mco_coro_free(mco_coro* k) {
-    return mco_coro_free(k);
-}
 
 char mco_coro_is_suspended(mco_coro* k) {
     return mco_status(k) == MCO_SUSPENDED;
-}
-char _mco_coro_is_suspended(mco_coro* k) {
-    return mco_coro_is_suspended(k);
 }
 
 char mco_coro_push(mco_coro* k, const void* arg, size_t size) {
@@ -39,17 +28,11 @@ char mco_coro_push(mco_coro* k, const void* arg, size_t size) {
     assert(res == MCO_SUCCESS);
     return 0;
 }
-char _mco_coro_push(mco_coro* k, const void* arg, size_t size) {
-    return mco_coro_push(k, arg, size);
-}
 
 char mco_coro_pop(mco_coro* k, void* arg, size_t size) {
     mco_result res = mco_pop(k, arg, size);
     assert(res == MCO_SUCCESS);
     return 0;
-}
-char _mco_coro_pop(mco_coro* k, void* arg, size_t size) {
-    return mco_coro_pop(k, arg, size);
 }
 
 char mco_coro_suspend(mco_coro* k) {
@@ -57,15 +40,9 @@ char mco_coro_suspend(mco_coro* k) {
     assert(res == MCO_SUCCESS);
     return 0;
 }
-char _mco_coro_suspend(mco_coro* k) {
-    return mco_coro_suspend(k);
-}
 
 char mco_coro_resume(mco_coro* k) {
     mco_result res = mco_resume(k);
     assert(res == MCO_SUCCESS);
     return 0;
-}
-char _mco_coro_resume(mco_coro* k) {
-    return mco_coro_resume(k);
 }
